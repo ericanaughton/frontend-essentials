83d5b4027c768155bc66b53f67f3acf9098d82d8
Buddhika Geasman <bgeasman@gmail.com>
2018-12-18 15:04:14 -0600

    Initial commit

---
+++ readme.md
@@ -0,0 +1,40 @@
+## To Do List
+
+- [ ] Bring past problems and current problems that need to be discussed.
+- [ ] At end of each mentor time, let me know what I need to improve.
+- [ ] Review
+
+# Javascript
+
+### Syntax
+
+- [x] Describe the difference between a string and a variable name
+- [x] Describe the difference between a variable and a function parameter
+- [x] Know when to use const or let
+- [x] Understand the scope of a code block
+- [x] Expressions and Operators
+- [x] Expressions vs Statement
+- [ ] Parameter vs Argument
+
+### Objects
+
+- [ ] Create a new object with object literal syntax
+- [ ] Access properties of an object when the property names are known (dot notation)
+- [ ] Access properties of an object when the property names are variable (bracket notation)
+- [ ] Modify the value for a property of an object (through both dot notation and bracket notation)
+- [ ] Create a new property for an object (through both dot notation and bracket notation)
+- [ ] Create a copy of an object with the spread operator
+- [ ] Add a new property when creating a copy with the spread operator
+- [ ] Use of Object.assign
+- [ ] Getters and setters
+
+# CSS
+
+- [ ] CSS grid
+- [ ] Flexbox
+
+### Notes to myself
+
+- [ ] Bring examples for each lesson.
+- [ ] Bring examples from our code base.
+- [ ] Be better at mentoring.
---
+++ src/syntax.test.js
@@ -0,0 +1,103 @@
+//difference between string and variable
+
+//a string is a value representing text
+//a variable is a name that can refer to any value
+
+//parameter vs argument
+//A parameter is a variable in a method definition. When a method is called,
+//the arguments are the data you pass into the method's parameters.
+
+const myMethod = parameter => {
+  return parameter;
+};
+const myArgument = 'I am the argument';
+myMethod(myArgument);
+
+//difference between variable and function parameter
+const name = 'Doran';
+
+it('should display undefined for name', () => {
+  const showName = name => {
+    return `Name is ${name}`;
+  };
+  //console.log(showName());
+  expect(showName()).toEqual('Name is undefined');
+});
+
+it('should display Doran for name', () => {
+  //using ES6 default parameter value
+  //in here name is a variable that is defined in global scope therefore I have access to it anywhere
+  // but personName is a parameter i defined in my function, which has no access outside my function scope
+  const showName = (personName = name) => {
+    return `Name is ${personName}`;
+  };
+
+  //console.log(showName());
+  expect(showName()).toEqual('Name is Doran');
+});
+
+const fixedStatus = 'Active';
+let status;
+
+const changeStatus = value => {
+  if (value % 2 === 0) {
+    return (status = 'Pending');
+  }
+
+  return (status = 'Complete');
+};
+
+it('should change the status to Pending', () => {
+  changeStatus(2);
+  //console.log(changeStatus(2));
+  expect(status).toEqual('Pending');
+});
+
+it('should change the status to Complete', () => {
+  changeStatus(3);
+  //console.log('now the status is ', changeStatus(3));
+  expect(status).toEqual('Complete');
+});
+
+//Can't run this test as fixedStatus is read only value
+// it('fixedStatus value should remian the same', () => {
+//   fixedStatus = 'Done';
+//   expect(fixedStatus).toEqual('Active');
+// });
+
+//function scope
+
+const foo = 'global scope';
+
+const bar = () => {
+  const baz = 'hello world';
+  return `show baz:${baz}, hey I am ${foo}`;
+};
+
+// it('should throw error when asked for baz, as it is in function scope', () => {
+//   expect(undefined).toEqual(baz);
+// });
+
+it('should display foo, as it is in global scope', () => {
+  console.log(foo);
+  expect(foo).toEqual('global scope');
+});
+
+it('now it should know about baz', () => {
+  bar();
+  expect(bar()).toEqual('show baz:hello world, hey I am global scope');
+});
+
+/**
+ * const foo = check whether value can be dived by 2
+ *                if so then operate on it
+ *                if not return the value
+ *
+ * const foo = value => value %2 === 0 ? value +1 : value
+ *
+ * operators +,-.%,/,* <=, >=
+ */
+
+const helloWorld = 'Hello World';
+
+//const hello = greeting => gretting isHappy ? beHappy : beSad
