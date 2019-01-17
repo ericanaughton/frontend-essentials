19d23dd2c333b8fd7ac8eb0737e8dc7093b7d2d9
Erica Miller <erica@eduphoria.net>
2019-01-17 11:24:40 -0600

    made changes
    Testing changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './styles.css';
+import "./styles.css";
 
 const myTable = [...Array(5).keys()]; //need to have 5 columns
 
@@ -11,7 +11,7 @@ function App() {
       <h1>Hello CodeSandbox</h1>
       <div className="table">
         {myTable.map((column, idx) => (
-          <div>Column {idx + 1}</div>
+          <div>Column {idx + 2}</div>
         ))}
       </div>
       <h2>Start editing to see some magic happen!</h2>
@@ -19,5 +19,5 @@ function App() {
   );
 }
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
